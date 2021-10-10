a4ddf4f3e9db6c14fab84d8a01af6c03aad90a09
Irokanade <michaeljs.leong@gmail.com>
2021-10-10 22:35:57 +0800

    Initial Commit

---
+++ forFun/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  forFun
+//
+//  Created by michaelleong on 10/10/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
